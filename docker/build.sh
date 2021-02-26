daf41d9f5c73fdc5fffd442fa04a0678f56ca41a
吴云 <browntea@gmail.com>
2021-02-26 21:31:56 +0800

    update hexo version and related package.json to 4.2.0
    Signed-off-by: 吴云 <browntea@gmail.com>

---
+++ docker/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IMAGE=harbor.hm.browntea.cn/customized/hexo
+TAG=4.2.0-node15-builder
+
+
+docker build -f Dockerfile.hexo -t $IMAGE:$TAG .
+
+echo "############################################################" 
+IMAGE=harbor.hm.browntea.cn/browntea/blog-hexo
+TAG=0.1
+
+mkdir -p .caches
+# rm -rf .caches/*
+
+# rsync -aP --exclude node_modules --exclude .git* ../* .caches/
+
+docker build -f Dockerfile -t $IMAGE:$TAG .
+
--- source/_posts/Jenkins-Pipeline.md
+++
@@ -1,5 +0,0 @@
----
-title: Jenkins Pipeline
-date: 2021-02-25 02:16:40
-tags:
----
